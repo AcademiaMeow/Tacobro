635cd9da63ef3ac72a8ced148ac4857ee7b4f340
黃志仁 <tbsthitw@gmail.com>
2018-12-27 13:52:01 +0800

    add simple template engine

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import request
 
+from views.template_engine import Template
 from views.board import BoardViews
 
 app = Flask(__name__)
@@ -16,8 +17,17 @@ def router(path):
     print(path)
     if 'board' == path.split('/')[0]:
         return BoardViews().list(request)
+
+    if 'template' == path.split('/')[0]:
+        return Template("index.html").render(request, {
+                "name": "Test",
+                "age": 20,
+                "ouo?": True,
+                "secret": "S3CR3T"
+            })
+
     return path
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
---
+++ views/template_engine.py
@@ -0,0 +1,38 @@
+import re
+
+class Template():
+    def __init__(self, file):
+        self.file = file
+
+    def render(self, request, params):
+        template_html = open(self.file).read()
+
+        def _gen_condition(matched):
+            condition = matched.group(1).strip()
+            if_statement = matched.group(2).strip()
+            else_statement = matched.group(3).strip()
+
+            if condition in params:
+                if type(params[condition]) != bool:
+                    raise ValueError(condition + " should be bool.")
+                return if_statement if params[condition] else else_statement
+            else:
+                return ""
+
+        template_html = re.sub("{%[\s]*if[\s]*(\S+)[\s]*%}([\s\S\n]*){%[\s]*else[\s]*%}([\s\S\n]*){% endif %}", _gen_condition, template_html)
+
+        def _get_value(matched):
+            key = matched.group(1)
+            return str(params[matched.group(1)]) if key in params else ""
+
+        template_html = re.sub(
+            "{{[ \t\n\r]*(\S+)[\s]*}}",
+            _get_value, template_html)
+
+        return template_html
+
+# Usage:
+# Template("index.html").render({
+#   "name": "Test",
+#   "age": 20
+# })
