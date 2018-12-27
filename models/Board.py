2603a2c73dacffa0c47b7d3fb6a0628369a11973
sleep2144985 <a26411277@gmail.com>
2018-12-28 04:56:35 +0800

    add models and create func

---
+++ models/Board.py
@@ -0,0 +1,8 @@
+from models.model import model
+class Board(model):
+
+    def __init__(self, name, description, bucket_list, manager):
+        self.name = name
+        self.description = description
+        self.bucket_list = bucket_list
+        self.manager = manager
