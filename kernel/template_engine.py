96dc9104bd376ef7a4b7425815cb69f4ac8ac7f4
黃志仁 <tbsthitw@gmail.com>
2018-12-30 23:16:40 +0800

    template engine -  add block

--- kernel/template_engine.py
+++ kernel/template_engine.py
@@ -4,8 +4,17 @@ class Template():
     def __init__(self, file):
         self.file = file
 
-    def render(self, request, params):
+    def render(self, request=None, params=dict()):
         template_html = open("templates/" + self.file).read()
+
+        extend = re.search(r'{%[\s]*extends[\s]*(\S+)[\s]*%}', template_html)
+        block = re.search(r'{%[\s]*block[\s]*%}([\s\S]*){%[\s]*endblock[\s]*%}', template_html)
+
+        if extend and block:
+            extend_html = Template(extend.group(1)).render(request, params)
+            template_html = re.sub(
+                r'{%[\s]*block[\s]*%}([\s\S]*){%[\s]*endblock[\s]*%}',
+                block.group(1), extend_html)
 
         def _gen_condition(matched):
             condition = matched.group(1).strip()
@@ -19,14 +28,16 @@ class Template():
             else:
                 return ""
 
-        template_html = re.sub("{%[\s]*if[\s]*(\S+)[\s]*%}([\s\S\n]*){%[\s]*else[\s]*%}([\s\S\n]*){% endif %}", _gen_condition, template_html)
+        template_html = re.sub(
+            r"{%[\s]*if[\s]*(\S+)[\s]*%}([\s\S\n]*){%[\s]*else[\s]*%}([\s\S\n]*){% endif %}",
+            _gen_condition, template_html)
 
         def _get_value(matched):
             key = matched.group(1)
             return str(params[matched.group(1)]) if key in params else ""
 
         template_html = re.sub(
-            "{{[ \t\n\r]*(\S+)[\s]*}}",
+            r"{{[ \t\n\r]*(\S+)[\s]*}}",
             _get_value, template_html)
 
         return template_html
