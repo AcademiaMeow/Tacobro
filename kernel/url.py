225a6f3df01eb202ff09ae7cb0a39e5c05cf5a92
HankLu <s210215@shsh.tw>
2018-12-31 01:12:45 +0800

    bug: url regex str

--- kernel/url.py
+++ kernel/url.py
@@ -3,6 +3,6 @@ import re
 
 def path(rule, view_f):
     rule = re.sub("<(?P<name>\w+):int>", "(?P<\g<name>>\d+)", rule)
-    rule = re.sub("<(?P<name>\w+):int>", "(?P<\g<name>>\d+)", rule)
+    rule = re.sub("<(?P<name>\w+):str>", "(?P<\g<name>>\w+)", rule)
     rule += '/?$'
     return rule, view_f
