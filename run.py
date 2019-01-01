5b3db380920d08ceff67bd0d31400dce32a33566
黃志仁 <tbsthitw@gmail.com>
2019-01-01 16:31:13 +0800

    Controller - Allow all method

--- run.py
+++ run.py
@@ -11,7 +11,7 @@ def root():
     return render_template("default.html")
 
 
-@app.route('/<path:path>')
+@app.route('/<path:path>', methods = ['GET', 'POST', 'PUT', 'DELETE'])
 def otherpath(path):
     return urls.dispatch(request, path)
 
