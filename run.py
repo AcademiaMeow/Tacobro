27761ad82277a4aca4c94afa96093c0230f9155b
黃志仁 <tbsthitw@gmail.com>
2019-01-02 16:00:45 +0800

    make session['username'] global

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import request, render_template
+from flask import request, render_template, redirect, session
 
 from controller import urls
 
@@ -8,17 +8,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def root():
-    return render_template("default.html")
+    return redirect("/board/八卦")
 
 
 @app.route('/<path:path>', methods = ['GET', 'POST', 'PUT', 'DELETE'])
 def otherpath(path):
+    request.username = session.get("username")
     return urls.dispatch(request, path)
 
 if __name__ == '__main__':
     import os
     app.secret_key = os.getenv('TACA_SECRET')
     # Generate secret_key:
-    # $ `python -c "import os, binascii;print(binascii.hexlify(os.urandom(24)))"`
+    # $ python -c "import os, binascii;print(binascii.hexlify(os.urandom(24)))"
     # $ export TACA_SECRET=***
     app.run(debug=True)
--- views/post.py
+++ views/post.py
@@ -1,8 +1,7 @@
 from flask import request, render_template
 
 def post(request, id):
-
-    return render_template("post.html", id=id)
+    return render_template("post.html", **locals())
 
 
 def meow(request, id, id2):
