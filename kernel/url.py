982e3c6de95465e1c16f86723b7bab3adf064a83
HankLu <s210215@shsh.tw>
2019-01-04 03:23:24 +0800

    admin required

--- kernel/url.py
+++ kernel/url.py
@@ -1,8 +1,8 @@
 import re
 
 
-def path(rule, view_f, login_required=False):
+def path(rule, view_f, login_required=False, admin_required=False):
     rule = re.sub("<(?P<name>\w+):int>", "(?P<\g<name>>\d+)", rule)
     rule = re.sub("<(?P<name>\w+):str>", "(?P<\g<name>>\w+)", rule)
     rule += '/?$'
-    return rule, view_f, login_required
+    return rule, view_f, login_required, admin_required
