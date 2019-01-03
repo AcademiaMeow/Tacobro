f106de9a795fd555195603c9dca70bc104ee0f46
HankLu <s210215@shsh.tw>
2019-01-03 22:03:44 +0800

    bug

--- models/User.py
+++ models/User.py
@@ -1,9 +1,10 @@
 from models.model import model
 from datetime import datetime
 
+
 class User(model):
 
-    def __init__(self, picture, username, password, first_name, last_name, birthday, join_date=datetime.now(), last_login, login_count, is_admin, is_active, profile):
+    def __init__(self, picture, username, password, first_name, last_name, birthday, last_login, login_count, is_admin, is_active, profile, join_date=datetime.now()):
         self.picture = picture
         self.username = username
         self.password = password
